62ef5d66f823f6de4480c24d71bde8b7f964c985
Christopher Mollard <christopher@j4gdesign.co.uk>
2018-12-07 16:29:50 +0000

    fixed it so that mens and womens foil now write to each sheet separately

--- Club list/clubranking.py
+++ Club list/clubranking.py
@@ -30,9 +30,9 @@ first_sheet = book.sheet_by_index(0)
 print first_sheet.col_values(1)
 
 #open the wf rankings spreadsheet
-book2 = xlrd.open_workbook('wf_rankings.xls')
+wf = xlrd.open_workbook('wf_rankings.xls')
 #create two new sheets in the new file
-second_sheet = book2.sheet_by_index(0)
+second_sheet = wf.sheet_by_index(0)
 #print the values in the second column of the first sheet
 print second_sheet.col_values(1)
 
@@ -41,40 +41,41 @@ print second_sheet.col_values(1)
 workbook = xlwt.Workbook()
 
 #add a sheet named "Club MF BFA ranking"
-worksheet1 = workbook.add_sheet("Club MF BFA ranking")
+mfClubRanking = workbook.add_sheet("Club MF BFA ranking")
 #in cell 0,0 (first cell of the first row) write "Ranking"
-worksheet1.write(0, 0, "Ranking")
+mfClubRanking.write(0, 0, "Ranking")
 #in cell 0,1 (second cell of the first row) write "Name"
-worksheet1.write(0, 1, "Name")  
+mfClubRanking.write(0, 1, "Name")  
 #in cell 0,2 (third cell of the first row) write "NIF"
-worksheet1.write(0, 2, "NIF")
+mfClubRanking.write(0, 2, "NIF")
 #in cell 0,1 (fourth cell of the first row) write "Points"
-worksheet1.write(0, 3, "Points") 
+mfClubRanking.write(0, 3, "Points") 
 #in cell 0,1 (fifth cell of the first row) write "Home country"
-worksheet1.write(0, 4, "Home country") 
+mfClubRanking.write(0, 4, "Home country") 
 
 #add a second sheet named "Club WF BFA ranking"
-worksheet2 = workbook.add_sheet("Club WF BFA ranking")
+wfClubRanking = workbook.add_sheet("Club WF BFA ranking")
 #in cell 0,0 (first cell of the first row) write "Ranking"
-worksheet2.write(0, 0, "Ranking")
+wfClubRanking.write(0, 0, "Ranking")
 #in cell 0,1 (second cell of the first row) write "Name"
-worksheet2.write(0, 1, "Name")  
+wfClubRanking.write(0, 1, "Name")  
 #in cell 0,2 (third cell of the first row) write "NIF"
-worksheet2.write(0, 2, "NIF")
+wfClubRanking.write(0, 2, "NIF")
 #in cell 0,1 (fourth cell of the first row) write "Points"
-worksheet2.write(0, 3, "Points") 
+wfClubRanking.write(0, 3, "Points") 
 #in cell 0,1 (fifth cell of the first row) write "Home country"
-worksheet2.write(0, 4, "Home country") 
+wfClubRanking.write(0, 4, "Home country") 
 
 #save and create the spreadsheet file
 workbook.save("saxons.xls")
+
+club = "Saxon"
 
 name = []
 rank = []
 NIF = []
 points = []
 hc = []
-club = "Saxon"
 
 for i in range(first_sheet.nrows):
     if(club in first_sheet.cell_value(i,3)):  
@@ -85,28 +86,34 @@ for i in range(first_sheet.nrows):
         hc.append (first_sheet.cell_value(i,6))
         print('a')
         
-for i in range(second_sheet.nrows):
-    if(club in second_sheet.cell_value(i,3)):  
-        name.append(second_sheet.cell_value(i,1))
-        rank.append(second_sheet.cell_value(i,0))
-        NIF.append (second_sheet.cell_value(i,9))
-        points.append (second_sheet.cell_value(i,10))
-        hc.append (second_sheet.cell_value(i,6))
-        print('b')
-        
 for j in range(len(name)):
-    worksheet1.write(j+1,0,rank[j])
-    worksheet1.write(j+1,1,name[j])
-    worksheet1.write(j+1,2,NIF[j])
-    worksheet1.write(j+1,3,points[j])
-    worksheet1.write(j+1,4,hc[j])
+    mfClubRanking.write(j+1,0,rank[j])
+    mfClubRanking.write(j+1,1,name[j])
+    mfClubRanking.write(j+1,2,NIF[j])
+    mfClubRanking.write(j+1,3,points[j])
+    mfClubRanking.write(j+1,4,hc[j])
     
-for k in range(len(name)):
+name = []
+rank = []
+NIF = []
+points = []
+hc = []
     
-    worksheet2.write(k+1,0,rank[k])
-    worksheet2.write(k+1,1,name[k])
-    worksheet2.write(k+1,2,NIF[k])
-    worksheet2.write(k+1,3,points[k])
-    worksheet2.write(k+1,4,hc[k])
+for j in range(second_sheet.nrows):
+    if(club in second_sheet.cell_value(j,3)):  
+        name.append(second_sheet.cell_value(j,1))
+        rank.append(second_sheet.cell_value(j,0))
+        NIF.append (second_sheet.cell_value(j,9))
+        points.append (second_sheet.cell_value(j,10))
+        hc.append (second_sheet.cell_value(j,6))
+        print('a')
+    
+for i in range(len(name)):
+    
+    wfClubRanking.write(i+1,0,rank[i])
+    wfClubRanking.write(i+1,1,name[i])
+    wfClubRanking.write(i+1,2,NIF[i])
+    wfClubRanking.write(i+1,3,points[i])
+    wfClubRanking.write(i+1,4,hc[i])
     
 workbook.save("saxons.xls")
